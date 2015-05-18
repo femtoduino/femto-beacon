d52fe7097aca57a9c93b07233e1673dd3203d1f9
Alex Albino <webmaster@alexventure.com>
2015-05-17 23:58:19 -0700

    Cleaning up the sam-r21-blink example

--- examples/sam-r21-blink/sam_r21_blink.c
+++ examples/sam-r21-blink/sam_r21_blink.c
@@ -1,8 +1,6 @@
 
 #include "asf.h"
-#include "conf_example.h"
 #include <port.h>
-#include <ioport.h>
 #include <delay.h>
 #include "sam0/utils/cmsis/samd21/include/pio/samd21e18a.h"
 
@@ -10,18 +8,30 @@
 
 #define MY_LED    PIN_PA08
 
+void configure_port_pins(void);
+void configure_port_pins(void)
+{
+  struct port_config config_port_pin;
+
+  port_get_config_defaults(&config_port_pin);
+  config_port_pin.direction = PORT_PIN_DIR_OUTPUT;
+
+  port_pin_set_config(MY_LED, &config_port_pin);
+}
+
 int main(void)
 {
   system_init();
   delay_init();
-  ioport_init();
-  ioport_set_pin_dir(MY_LED, IOPORT_DIR_OUTPUT);
+
+  configure_port_pins();
 
   bool isRunning = true;
+  bool pin_state = false;
 
   do {
-
-    ioport_toggle_pin_level(MY_LED);
+      pin_state = !pin_state;
+      port_pin_set_output_level(MY_LED, pin_state);
     delay_ms(500);
   } while (isRunning == true);
 }
